a0d49c099e95f78b04e8e75628425061cbbb1814
Ricky Sandoval <rsandoval@arrow.com>
2015-10-19 21:07:36 -0600

    Absolute sloth

--- js/components/Header.react.js
+++ js/components/Header.react.js
@@ -4,7 +4,7 @@ var Header = React.createClass({
 	render: function() {
 		return (
 			<header className="site-header">
-				<h1 className="site-header__title">Color thing</h1>
+				<h1 className="site-header__title">Color Pane</h1>
 			</header>
 		);
 	}
